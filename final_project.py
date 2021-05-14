5b628241daff789668487344fbd89c690b04bff2
alex5388 <alexmiller5388@gmail.com>
2021-05-14 10:14:57 -0400

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -1,18 +1,37 @@
 from flask import *
 import requests
 import json
+from bs4 import BeautifulSoup
 
-book_isbn = '9780545010221'
+#scrape website
+
+book_isbn = '9781449372620'
 r = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:{}".format(book_isbn))
-print(r.url)
+soup = BeautifulSoup(r.content, 'html.parser')
 
-app = Flask(__name__)
+'''Parsed JSON Data'''
 
-@app.route('/')
-def hello():
-    return r.json()
+data = json.loads(soup.text)
+try:
+    title = data['items'][0]['volumeInfo']['title']
+    print(title)
+except KeyError:
+    print('***TITLE COULD NOT BE FOUND***')
 
+try:
+    author = data['items'][0]['volumeInfo']['authors'][0]
+    print(author)
+except KeyError:
+    print('***AUTHOR COULD NOT BE FOUND***')
 
+try:
+    page_count = data['items'][0]['volumeInfo']['pageCount']
+    print(page_count)
+except KeyError:
+    print('***PAGE COUNT COULD NOT BE FOUND***')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+try:
+    average_rating = data['items'][0]['volumeInfo']['averageRating']
+    print(average_rating)
+except KeyError:
+    print('***AVERAGE RATING COULD NOT BE FOUND***')
