2851f3aa58f0ede8df721b0e0fba11bf906c4873
alex5388 <alexmiller5388@gmail.com>
2021-05-14 15:10:32 -0400

    Add files via upload

--- final_project.py
+++ final_project.py
@@ -2,35 +2,77 @@ from flask import *
 import requests
 import json
 from bs4 import BeautifulSoup
+import sqlite3
+import os
 
-# Scrape website
-book_isbn = '9781449372620'
-r = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:{}".format(book_isbn))
-soup = BeautifulSoup(r.content, 'html.parser')
+app = Flask(__name__)
+app.secret_key = ('pass')
 
 
-# Parse JSON data
-data = json.loads(soup.text)
-try:
-    title = data['items'][0]['volumeInfo']['title']
-    print(title)
-except KeyError:
-    print('***TITLE COULD NOT BE FOUND***')
+#login form
+'''@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin':
+            error = 'Invalid Username or Password.'
+            return render_template('login.html', error=error)
+        elif request.form['password'] != 'password':
+            error = 'Invalid Username of Password.'
+            return render_template('login.html', error=error)
+        else:
+            #start session
+            session['logged_in'] = True
+            if session.get('logged_in') != None:
+                print('session started')
+            return redirect('/maindisplay')
+    else:
+        return render_template('login.html', error=error)'''
 
-try:
-    author = data['items'][0]['volumeInfo']['authors'][0]
-    print(author)
-except KeyError:
-    print('***AUTHOR COULD NOT BE FOUND***')
+# Main Display
+@app.route('/maindisplay', methods=['GET'])
+def books():
 
-try:
-    page_count = data['items'][0]['volumeInfo']['pageCount']
-    print(page_count)
-except KeyError:
-    print('***PAGE COUNT COULD NOT BE FOUND***')
+    #db connection
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/books.db')
+    c = conn.cursor()
 
-try:
-    average_rating = data['items'][0]['volumeInfo']['averageRating']
-    print(average_rating)
-except KeyError:
-    print('***AVERAGE RATING COULD NOT BE FOUND***')
+    # Select statements to retrieve book table data
+    c.execute("SELECT * FROM books")
+    books_info = c.fetchall()
+
+    return render_template('maindisplay.html', books_info=books_info)
+
+
+#function to add books to account
+@app.route('/search', methods=['GET','POST'])
+def search():
+
+    #variable for html form
+    isbn = request.form["isbn"]
+
+    # Scrape website
+    #book_isbn = '9781449372620'
+    r = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:{}".format(isbn))
+    soup = BeautifulSoup(r.content, 'html.parser')
+    data = json.loads(soup.text)
+    try:
+        title = data['items'][0]['volumeInfo']['title']
+        print(title)
+    except KeyError:
+        print('***TITLE COULD NOT BE FOUND***')
+
+
+
+    #path = os.path.dirname(os.path.abspath(__file__))
+    #conn = sqlite3.connect(path+'/books.db')
+    #c = conn.cursor()
+    #c.execute("INSERT INTO books (isbn) VALUES (?)",[isbn])
+    #conn.commit()
+
+    return render_template('search.html', title=title)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
